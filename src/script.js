b660a72498f4091d18bc5dcb529067e0e953d0ef
Tyler Maier <tyler_maier@yahoo.com>
2016-08-14 16:16:38 -0600

    finishes basic checkerboard

--- src/script.js
+++ src/script.js
@@ -1,27 +1,26 @@
-// function addElement (grid)  {
-//
-//
-//   for (var i = 0; i < grid; i++) {
-//     for (var j = 0; j < grid; j++) {
-//       if ((i+j)%2 === 0) {
-//         var check = document.createElement('div');
-//         document.body.appendChild(check);
-//         check.style.width = 100/grid + '%';
-//         check.style.backgroundColor = '#000000';
-//         check.style.paddingBottom = 100/grid + '%';
-//         check.style.float = 'left';
-//       } else {
-//         var check = document.createElement('div');
-//         document.body.appendChild(check);
-//         check.style.width = 100/grid + '%';
-//         check.style.backgroundColor = 'red';
-//         check.style.paddingBottom = 100/grid + '%';
-//         check.style.float = 'left';
-//         }
-//       }
-//     }
-//   }
-// addElement(9)
+function addElement (grid)  {
+
+  for (var i = 0; i < grid; i++) {
+    for (var j = 0; j < grid; j++) {
+      if ((i+j)%2 === 0) {
+        var check = document.createElement('div');
+        document.body.appendChild(check);
+        check.style.width = 100/grid + '%';
+        check.style.backgroundColor = '#000000';
+        check.style.paddingBottom = 100/grid + '%';
+        check.style.float = 'left';
+      } else {
+        var check = document.createElement('div');
+        document.body.appendChild(check);
+        check.style.width = 100/grid + '%';
+        check.style.backgroundColor = 'red';
+        check.style.paddingBottom = 100/grid + '%';
+        check.style.float = 'left';
+        }
+      }
+    }
+  }
+addElement(9)
 
 // function randomBoard (grid) {
 //   for (var i = 0; i < grid*grid; i++) {
@@ -47,22 +46,22 @@
 //   // console.log("#" + color);
 // }, 1000);
 // }
-randomBoard(9)
+// randomBoard(9)
+// //
+//   function randomBoard (grid) {
 //
-  function randomBoard (grid) {
-
-
-    for (var i = 0; i < 81; i++) {
-      var color = Math.floor(Math.random()*16777215).toString(16);
-      var check = document.createElement('button');
-        check.style.backgroundColor = '#'+ color;
-        check.style.width = 100/grid + '%';
-        check.style.paddingBottom = 100/grid + '%';
-        check.style.float = 'left';
-        document.body.appendChild(check);
-      }
+//
+//     for (var i = 0; i < 81; i++) {
+//       var color = Math.floor(Math.random()*16777215).toString(16);
+//       var check = document.createElement('button');
+//         check.style.backgroundColor = '#'+ color;
+//         check.style.width = 100/grid + '%';
+//         check.style.paddingBottom = 100/grid + '%';
+//         check.style.float = 'left';
+//         document.body.appendChild(check);
+//       }
     //   var color = Math.floor(Math.random()*16777215).toString(16);
     // setInterval(function () {
     //   'button'.style.backgroundColor = '#'+ color;
     // }, 1000);
-}
+// }
